fe540b60dbee30134a3aae7569479ea39b4f5824
mrdragar <yshhenyaev@mail.ru>
2025-07-23 23:27:15 +0300

    Fix success message

--- src/app/main.py
+++ src/app/main.py
@@ -39,4 +39,4 @@ async def create_pdf(report: LDPRReport, request: Request):
     report_filename = f"report_{uuid.uuid4()}.pdf"
     report_filepath = os.path.join("media", report_filename)
     generate_pdf_report(report.dict(), report_filepath)
-    return {"status": "Succes", "message": f"{request.base_url}media/{report_filename}"}
+    return {"status": "Success", "message": f"{request.base_url}media/{report_filename}"}
